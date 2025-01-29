6c929be39e3107b08f57199070efcabddb64bd89
Yoel Francés <yoelfrances.alu@iespacomolla.es>
2025-01-29 12:20:45 +0100

    prueba2

--- gestor_transformaciones/__manifest__.py
+++ gestor_transformaciones/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': 'Gestor de Transformaciones',
     'version': '16.0.1.0.0',
-    'category': 'Custom',
+    'category': 'Tools',
     'description': 'Este módulo permite gestionar transformaciones de Dragon Ball',
     'author': 'Yoel Francés & Elena Montserrat',
     'website': 'https://www.iespacomolla.es',
